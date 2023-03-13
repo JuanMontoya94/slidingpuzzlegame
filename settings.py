8847cd55ef257b3166223c9e893ec5ac79053d61
JuanMontoya94 <juand.montoyam@uqvirtual.edu.co>
2023-03-12 22:23:10 -0500

    primer commit

---
+++ settings.py
@@ -0,0 +1,19 @@
+# COLORS (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BLUE = (60, 207, 255)
+GREY = (181, 178, 178)
+BURLYWOOD = (222, 184, 135)
+WHEAT = (245, 222, 179)
+BROWN = (139, 69, 19)
+BGCOLOUR = BURLYWOOD
+
+# game settings
+WIDTH = 1200
+HEIGHT = 650
+FPS = 60
+title = "Juego rompecabezas 4 x 4 - Parcial IA Uniquindio"
+TILESIZE = 128
+GAME_SIZE = 4
